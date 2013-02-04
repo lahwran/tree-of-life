1c5052e7a98895ef8140346e9222983e56a5653f
Chris Horne <lahwran@lahwran.net>
2013-02-03 19:35:36 -0700

    Add alarmclock command as temporary workaround

---
+++ bin/_preamble.py
@@ -0,0 +1,34 @@
+# This file Copyright (c) Twisted Matrix Laboratories.
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+This makes sure that users don't have to set up their environment
+specially in order to run these programs from bin/.
+"""
+
+
+import sys, os
+
+path = os.path.abspath(os.path.realpath(sys.argv[0]))
+while os.path.dirname(path) != path:
+    if os.path.exists(os.path.join(path, 'todo_tracker', '__init__.py')):
+        sys.path.insert(0, path)
+        break
+    path = os.path.dirname(path)
