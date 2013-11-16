c506562750b44fd1e01d8d763d8debec386c644e
Chris Horne <lahwran@lahwran.net>
2013-11-16 12:52:54 -0700

    Add classifiers to setup.py, switch to simpler version

--- setup.py
+++ setup.py
@@ -3,12 +3,30 @@ from setuptools import setup, find_packages
 setup(
         name="treeoflife",
         description="A Tree of Life",
-        version="0.2",
+        version="1",
         packages=find_packages(),
         license='MIT',
         author="lahwran",
         author_email="lahwran0@gmail.com",
         scripts=["bin/todo-tracker"],
         install_requires=["twisted", "parsley", "pytest", "pep8", "txws",
-            "raven"]
+            "raven"],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Environment :: MacOS X',
+            'Framework :: Twisted',
+            'Intended Audience :: End Users/Desktop',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Operating System :: MacOS :: MacOS X',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 2 :: Only',
+            'Programming Language :: Python :: Implementation :: PyPy',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Programming Language :: Objective C',
+            'Topic :: Office/Business :: Scheduling',
+            'Topic :: Software Development :: Bug Tracking'
+        ],
+        keywords=['todo', 'planning', 'scheduling', 'task', 'concentration']
 )
