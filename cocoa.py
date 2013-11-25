1936ddf3f8610e95057998fbd7398569c84d0bcf
Chris Horne <lahwran@lahwran.net>
2013-11-25 07:59:12 -0700

    Be a bit more defensive in some hacky code

--- cocoa.py
+++ cocoa.py
@@ -235,12 +235,17 @@ class JSONProtocol(LineOnlyReceiver):
             root = self.commandline.root
             pool = root.ui_graph()
             active_ref = getattr(root.active_node, "_px_root", None)
+            todo = getattr(root, "todo", None)
+            if todo is not None:
+                todo_id = todo.id
+            else:
+                todo_id = None
             pool["ids"] = {
                 "root": root.id,
                 "days": "00001",
                 "active": root.active_node.active_id,
                 "active_ref": active_ref.id if active_ref else None,
-                "todo_bucket": root.todo.id
+                "todo_bucket": todo_id
             }
             self.sendmessage({
                 "promptnodes": [node.id for node in reversed_displaychain],
