82e50bb343be9cab163cc34548b12ea3090fd42e
Chris Horne <lahwran@lahwran.net>
2014-02-22 15:23:23 -0700

    Re-add removed debug utility

---
+++ treeoflife/interact_searching.py
@@ -0,0 +1,44 @@
+from __future__ import unicode_literals, print_function
+
+if __name__ == "__main__":
+    import sys
+    from treeoflife import searching
+    if "create" in sys.argv:
+        dothing = searching.parse_create
+    else:
+        dothing = searching.parse
+
+    todofile = "/Users/lahwran/.treeoflife/life"
+    from treeoflife.tracker import Tracker
+    import time
+    tracker = Tracker()
+    with open(todofile, "r") as reader:
+        tracker.deserialize("file", reader)
+
+    while True:
+        querytext = raw_input("query: ")
+        import subprocess
+        subprocess.call(["clear"])
+        print("query:", querytext)
+        a = time.time()
+        queryer = dothing(querytext)
+        b = time.time()
+        print(queryer, b - a)
+
+        inittime = time.time()
+        results = queryer(tracker.root)
+        finishtime = time.time()
+
+        print()
+
+        if hasattr(results, "list"):
+            results = results.list()
+            for x in results[:1000]:
+                print( " > ".join([
+                    str(node) for node in x.iter_parents()][::-1]))
+            print()
+            print(len(results), finishtime - inittime)
+        else:
+            print(results)
+            print()
+            print(1, finishtime - inittime)
