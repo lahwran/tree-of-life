8b021b03b4a0c15a00b50d1d11f426183645c3d5
Chris Horne <lahwran@lahwran.net>
2014-03-18 20:05:13 -0600

    Don't close windows terminal windows until told to

--- bin/treeoflife-server.py
+++ bin/treeoflife-server.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 # windows version
-import os, sys
-
 try:
-    import _preamble
-except ImportError:
-    sys.exc_clear()
+    import os, sys
 
-import treeoflife.tracker # primary module entry point
-from treeoflife import main
-main._main()
+    try:
+        import _preamble
+    except ImportError:
+        sys.exc_clear()
+
+    import treeoflife.tracker # primary module entry point
+    from treeoflife import main
+    main._main()
+
+finally:
+    print()
+    print()
+    raw_input("        Press enter to close\n\n ->")
