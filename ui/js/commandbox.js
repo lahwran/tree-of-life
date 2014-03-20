5c211827395f86a8fab66ea7fc8a366ea4b2a3e8
Chris Horne <lahwran@lahwran.net>
2014-03-19 20:42:36 -0600

    Implement history in new ui

--- ui/js/commandbox.js
+++ ui/js/commandbox.js
@@ -1,27 +1,46 @@
 angular.module("treeoflife")
-.directive("tlCommandbox", function() {
+.directive("tlCommandbox", function($rootScope, backend) {
     return {
         restrict: "E",
-        template: '<input type="text" ng-model="command" class="commandbox reset" tl-keypress="keys" autofocus>',
+        template: '<input type="text" ng-model="command" ng-change="changed" class="commandbox reset" tl-keypress="keys" autofocus>',
         replace: true,
         scope: {
             sendCommand: "="
         },
         link: function($scope, $element, $attrs) {
             $scope.keys = {
+                // enter
                 13: function() {
                     var c = $scope.command;
                     $scope.command = "";
                     $scope.sendCommand(c);
                 },
+                // home
                 35: function() {
                     var l = $scope.command.length;
                     $element[0].setSelectionRange(l, l);
                 },
+                // end
                 36: function() {
                     $element[0].setSelectionRange(0, 0);
+                },
+                // up
+                38: function() {
+                    backend.send({navigate: "up"});
+                },
+                // down
+                40: function() {
+                    backend.send({navigate: "down"});
                 }
             }
+            $scope.changed = function() {
+                if (angular.isDefined($scope.command)) {
+                    backend.send({input: $scope.command});
+                }
+            }
+            $rootScope.$on("message/input", function(event, value) {
+                $scope.command = value;
+            });
         }
     };
 });
